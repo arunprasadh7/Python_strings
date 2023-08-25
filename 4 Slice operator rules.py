3f5c1911f308f9b2a66aee7a3161b0b2b8fab98f
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-08-25 21:23:33 +0530

    slice operator

---
+++ 4 Slice operator rules.py
@@ -0,0 +1,15 @@
+#Rules for slice operator
+
+'''
+s[start:stop:step]
+
+for +ve index:
+from left to right
+start to end-1
+if end value is 0 result is empy
+
+for -ve index: 
+- right to left 
+- start to end+1
+- if end value is -1 result is empty
+'''
