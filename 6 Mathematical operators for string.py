d42b377dd57945675beb99a6077d04909223c00c
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-08-26 11:48:29 +0530

    math operators

---
+++ 6 Mathematical operators for string.py
@@ -0,0 +1,14 @@
+#Mathematical Operators for String:
+
+# + - concatenation - can only add strings
+
+a = 'arun'
+b ='arun'
+print(a+b)
+
+# * - repeat - 1 val must be int and other must be string
+
+a = 'arun' * 3
+b = 3 * 'iron' 
+print(a)
+print(b)
